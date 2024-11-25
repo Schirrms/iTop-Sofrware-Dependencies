723fd1607f5db5161afac3b69cc25f1fc4d3cbe3
Schirrms <schirrms@schirrms.net>
2024-11-25 18:11:52 +0100

    ✨ First working version

--- schirrms-software-dependencies/en.dict.schirrms-software-dependencies.php
+++ schirrms-software-dependencies/en.dict.schirrms-software-dependencies.php
@@ -8,5 +8,14 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class::FunctionalCI/Attribute:softwareinstance_id' => 'Software depending on me',
+	'Class::FunctionalCI/Attribute:softwareinstance_name' => 'Software depending on me name',
+	'Class:SoftwareInstance/Attribute:ci_depends_list' => 'Needed item(s)',
+  'Class:SoftwareInstance/Attribute:ci_depends_list+' => 'This item is mandatory for operation',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy' => 'Redundancy mode (normally set at 1, as \'really needed\')',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy/disabled' => 'All listed items must be up & running',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy/percent' => '%2$s is up if at least %1$s %% of needed items are up',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy/count' => '%2$s is up if at least %1$s needed item(s) is/are up',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy+' => 'Generally speaking, this should probably be set at 100%',
 ));
 ?>
---
+++ schirrms-software-dependencies/fr.dict.schirrms-software-dependencies.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2024 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+ Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class::FunctionalCI/Attribute:softwareinstance_id' => 'Logiciel(s) dépendant(s)',
+	'Class::FunctionalCI/Attribute:softwareinstance_name' => 'Logiciel(s) dépendant(s)',
+	'Class:SoftwareInstance/Attribute:ci_depends_list' => 'Éléments(s) nécessaire(s)',
+  'Class:SoftwareInstance/Attribute:ci_depends_list+' => 'Cet(s) CI(s) est(sont) nécessaires pour que le logiciel puisse fonctionner',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy' => 'Mode  de redondance (normallement désactivé, car \'indispensable\')',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy/disabled' => 'Tous les composants listés sont nécessaires simultanément',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy/percent' => '%2$s est opérationnel si au moins %1$s %% des éléments sous-jacents sont opérationnels',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy/count' => '%2$s est opérationnel si au moins %1$s élément(s) sous-jacent(s) est/sont opérationnel(s)',
+  'Class:SoftwareInstance/Attribute:secondary_redundancy+' => 'La valeur la plus probable est \'redondance  désactivée\'',
+));
+?>
